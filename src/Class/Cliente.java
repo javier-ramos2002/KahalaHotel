235ca559c60e1a10cba115e2d805e70050f8d36c
alvaromartinezu <alvaro.martinezu@opendeusto.es>
2022-10-24 22:12:10 +0200

    class client

--- src/Class/Cliente.java
+++ src/Class/Cliente.java
@@ -1,5 +1,56 @@
 package Class;
 
-public class Cliente {
 
+
+public class Cliente {
+    private String nom;
+    private String dni;
+    private String con;
+    private long fechanacim;
+
+    public Cliente(String nom, String dni, String con, long fechanacim){
+        super();
+        this.con = con;
+        this.nom = nom;
+        this.dni = dni;
+        this.fechanacim = fechanacim;
+    }
+
+    public void setCon(String con) {
+		this.con = con;
+	}
+    public String getCon() {
+		return con;
+	}
+    public void setNom(String nom) {
+		this.nom = nom;
+	}
+    public String getNom() {
+		return nom;
+	}
+    public void setDni(String dni) {
+		this.dni = dni;
+	}
+    public String getDni() {
+		return dni;
+	}
+    public void setFechanacim(long Fechanacim) {
+		this.fechanacim = fechanacim;
+
+	}
+    public long getFechanacim() {
+		return fechanacim;
+	}
+
+    public String toString(){
+        return nom + ',' + dni + ',' + con + ',' + fechanacim;
+    }
+
+
+
+
+
+
+    
+    
 }
