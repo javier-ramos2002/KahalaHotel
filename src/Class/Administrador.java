ca81d2ee8454814d99971bbb11fd39a2f6488429
alvaromartinezu <alvaro.martinezu@opendeusto.es>
2022-10-24 22:33:01 +0200

    class administrador

---
+++ src/Class/Administrador.java
@@ -0,0 +1,33 @@
+package Class;
+
+import java.io.Serializable;
+
+public class Administrador{
+    
+    private String dni;
+    private String contrasenya;
+
+    public Administrador(String dni, String contrasenya){
+       
+        super();
+        this.dni = dni;
+        this.contrasenya = contrasenya;
+    }
+
+    public String getDni() {
+		return dni;
+	}
+
+    public void setContrasenya(String contrasenya) {
+		this.contrasenya = contrasenya;
+	}
+	
+    public String getContrasenya() {
+		return contrasenya;
+	}
+
+    @Override
+    public String toString(){
+        return  dni + ',' + contrasenya ;
+    }
+}
