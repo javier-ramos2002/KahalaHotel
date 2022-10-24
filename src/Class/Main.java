243485e8092d30556ec0c87ebd5a8e408852df97
alvaromartinezu <alvaro.martinezu@opendeusto.es>
2022-10-24 21:57:03 +0200

    commit alvaro

--- src/Class/Main.java
+++ src/Class/Main.java
@@ -4,6 +4,7 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("Prueba");
 		System.out.println("Prueba Pele");
+		System.out.println("Alvaro Commit");
 		System.out.println("Alvaro");
 	}
 }
